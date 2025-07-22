31d69626f89d23962512991cae5f508926837733
divyaprakash123 <kishori.divya@gmail.com>
2025-07-21 19:10:50 -0500

    initial setup of site framework/page of all the sections and added to the main app page in order

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { onMount } from 'svelte';
+  import TitlePage from './sections/TitlePage.svelte';
+  import Headlines from './sections/Headlines.svelte';
+  import Stories from './sections/Stories.svelte';
+  import Data from './sections/Data.svelte';
+  import Comparison from './sections/Comparison.svelte';
+  import HealthOutcomes from './sections/HealthOutcomes.svelte';
+  import Solutions from './sections/Solutions.svelte';
+  import CallToAction from './sections/CallToAction.svelte';
+
+  
